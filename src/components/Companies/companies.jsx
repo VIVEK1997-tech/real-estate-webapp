9dcca20f10ab95c5fa8ba04f23f882d7cb396619
Vivek yadav <yadav.0101vivek@gmail.com>
2023-08-11 09:52:07 +0530

    intial commit

---
+++ src/components/Companies/companies.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./companies.css";
+const companies = () => {
+  return (
+    <section className="c-wrapper">
+      <div className="padding innerWidth flexCenter c-container">
+        <img src="./prologis.png" alt="c" />
+        <img src="./tower.png" alt="g" />
+        <img src="./equinix.png" alt="f" />
+        <img src="./realty.png" alt="s" />
+      </div>
+    </section>
+  );
+};
+
+export default companies;
